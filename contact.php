cfc393f17b595e3f7e1afc9884a6dd0ea777d401
Steph-Gam <gamboastephanie@live.com>
2017-01-30 02:04:17 -0800

    Changes

--- contact.php
+++ contact.php
@@ -6,6 +6,13 @@ $formcontent=" From: $name \n Message: $message";
 $recipient = "gamboastephanie@live.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+if( mail($recipient, $subject, $formcontent, $mailheader) )
+    {
+        echo "<h2>Thank you for your comment</h2>";
+    }
+    else
+    {
+        echo "<h2>Sorry, there has been an error</h2>";
+    }
+
 ?>
